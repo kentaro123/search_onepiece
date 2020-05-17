2a2b7a5bc1ec1962a78449d1a7fbc15febae70cd
kentaro123 <kentaro.w0918@gmail.com>
2020-05-17 21:29:01 +0900

    heroku

--- views.py
+++ views.py
@@ -10,15 +10,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    str_out = ""
-    str_out += "<h2>Hello from Python!</h2>"
-    str_out += "<blockquote>"
-    str_out += "こんにちは<p />"
-    str_out += "</blockquote>"
-    str_out += "Aug/07/2017 PM 12:49<br />"
-#
-    return str_out
+def index():
+    return render_template("login.html")
 #
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
