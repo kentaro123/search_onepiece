4931c2da4bdba07e1e009e4fec04a17109f11809
kentaro123 <kentaro.w0918@gmail.com>
2020-05-17 21:43:35 +0900

    heroku

--- server/views.py
+++ server/views.py
@@ -1,31 +1,20 @@
-from flask import jsonify
-from conf import *
-from flask import Flask, render_template
-from flask import request
-from server.run_server import app
-from server.module import KnowGraph
-import json
+# -*- coding: utf-8 -*-
+# -------------------------------------------------------------------
+#
+#   hello.py
+#
+#                       Aug/07/2017
+# -------------------------------------------------------------------
 import os
-
-
-
+from flask import Flask, render_template
 app = Flask(__name__)
-app.debug = True
 
 @app.route("/")
 def index():
     return render_template("login.html")
-
-"""
-@app.route('/result',methods=["POST","GET"])
-def look_up():
-    kg = KnowGraph()
-    client_params = request.form
-    server_param = {}
-    cont = kg.lookup_entry(client_params,server_param)
-    return render_template("result.html",cont=cont)
-"""
-
-if __name__ == '__main__':
+#
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
+# -------------------------------------------------------------------
